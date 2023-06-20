493e278a86cfc7503c352b8c17ecf9564dabb4c9
Ben Holmes <ben.holmes@hashicorp.com>
2023-06-20 10:37:22 +0100

    ternary expression for features

--- main.tf
+++ main.tf
@@ -6,9 +6,9 @@ resource "azuredevops_project" "project" {
   work_item_template = "Agile"
 
   features = {
-    "boards"       = var.feature_boards
-    "repositories" = var.feature_repositories
-    "pipelines"    = var.feature_pipelines
-    "artifacts"    = var.feature_artifacts
+    "boards"       = var.feature_boards == true ? "enabled" : "disabled"
+    "repositories" = var.feature_repositories == true ? "enabled" : "disabled"
+    "pipelines"    = var.feature_pipelines == true ? "enabled" : "disabled"
+    "artifacts"    = var.feature_artifacts == true ? "enabled" : "disabled"
   }
 }
